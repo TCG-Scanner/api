8b766c5de3af9155c5875e4be0fcb45fdf5b668b
John Dinh <johnduydinh@gmail.com>
2022-02-25 00:34:17 -0500

    load .env file variables

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
+from os import getenv
 from PIL import Image
 from pytesseract import pytesseract
+from dotenv import load_dotenv
+load_dotenv()  # take environment variables from .env.
 
 image_path = "test.jpg"
 
 img = Image.open(image_path)
 
-#  pytesseract.tesseract_cmd = path
+if (tesseract_path := getenv("TESSERACT_PATH")) is not None:
+    pytesseract.tesseract_cmd = tesseract_path
 
 test = pytesseract.image_to_string(img)
 print(test)
